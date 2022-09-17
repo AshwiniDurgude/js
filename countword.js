a50ef6b8fab060e9322051dfe10b4e571778c241
AshwiniDurgude <71395390+AshwiniDurgude@users.noreply.github.com>
2022-09-17 19:07:19 +0530

    Add files via upload

---
+++ countword.js
@@ -0,0 +1,5 @@
+function countword(str)
+{
+    return str.split('').length
+}
+console.log(countword("hi are you there"))
---
+++ hello.js
@@ -0,0 +1,14 @@
+var a="vbf";
+var b=3;
+
+function hello()
+{
+    return "hello world"
+}
+// document.write(hello())
+
+function add( x ,t)
+{
+    return x+t
+}
+console.log(add(a,b))
